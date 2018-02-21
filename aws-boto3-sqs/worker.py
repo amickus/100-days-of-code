52719d1a9d4fd5ae6d1db9ad10ed6bc13dde3720
Auris <aurimasmic@yahoo.com>
2018-02-21 21:18:26 +0000

    R1D14 Boto3, sqs, json

--- aws-boto3-sqs/worker.py
+++ aws-boto3-sqs/worker.py
@@ -2,6 +2,14 @@
 
 import boto3
 import time
+import json
+
+
+def processor(message):
+    processed_message = message + '-Proccessed'
+    processed = json.dumps({'key1': message, 'key2': processed_message})
+    return processed
+
 
 # Get the service resource
 sqs = boto3.resource('sqs', region_name='eu-west-1')
@@ -12,6 +20,7 @@ sqs = boto3.resource('sqs', region_name='eu-west-1')
 
 # Get the queue. This returns an SQS.Queue instance
 queue = sqs.get_queue_by_name(QueueName='auri-sqs-test')
+queue_processed = sqs.get_queue_by_name(QueueName='auri-sqs-test-processed')
 # queue = sqs.get_queue_by_name(QueueName='auri-demo-02-image-processor')
 
 # Create a new message
@@ -21,10 +30,14 @@ queue = sqs.get_queue_by_name(QueueName='auri-sqs-test')
 # print(response.get('MessageId'))
 # print(response.get('MD5OfMessageBody'))
 
-
 while 1:
     messages = queue.receive_messages(WaitTimeSeconds=5)
     for message in messages:
-        print("Message Deleted: {0}".format(message.body))
+        print("Processing message: {0}".format(message.body))
+        response = queue_processed.send_message(
+            MessageBody=processor(message.body))
+        print ('Moving {} to queue {}'.format(
+            processor(message.body), queue_processed))
+        print("Deleting message: {0}".format(message.body))
         message.delete()
         time.sleep(1)
--- r1-log.md
+++ r1-log.md
@@ -42,3 +42,6 @@ R1D12 Boto3 and sqs
 
 ### R1D13
 R1D13 Boto3 and sqs 2
+
+### R1D14
+R1D14 Boto3, sqs, json
